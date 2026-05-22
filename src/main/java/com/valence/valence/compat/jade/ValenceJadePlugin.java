535f81671da87731438152d8ad85d6a9310d5eef
karakulakdede32-ui <2.39968495e+08+karakulakdede32-ui@users.noreply.github.com>
2026-05-22 21:08:07 +0000

    Final: Added Grinder recipe and Jade/EMI compatibility placeholders

---
+++ src/main/java/com/valence/valence/compat/jade/ValenceJadePlugin.java
@@ -0,0 +1,14 @@
+package com.valence.valence.compat.jade;
+
+import com.valence.valence.block.miner.BasicMinerTileEntity;
+import com.valence.valence.block.miner.AdvancedMinerTileEntity;
+import snownee.jade.api.*;
+import snownee.jade.api.config.IPluginConfig;
+
+@WailaPlugin
+public class ValenceJadePlugin implements IWailaPlugin {
+    @Override
+    public void register(IWailaClientRegistration registration) {
+        // Registration for Miner info in Jade
+    }
+}
